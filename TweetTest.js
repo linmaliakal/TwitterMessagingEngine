dd15622b096d09fa87502ed67f8bd873cbaf6801
macabrera9 <mcabrera2@luc.edu>
2019-01-25 16:08:48 -0600

    adding proper .js and styles sheet
    Signed-off-by: macabrera9 <mcabrera2@luc.edu>

---
+++ TweetTest.js
@@ -0,0 +1,47 @@
+function testTweet(){;
+  var tweet = new Object(), txt = document.getElementById("field1");
+  var tweetLength = txt.value.length;
+  if (tweetLength > 280){
+    document.getElementById("validity").innerHTML = "This is not a valid tweet";
+    document.getElementById("length").innerHTML = txt.value.length;
+  }
+  if (tweetLength == 0){
+    document.getElementById("validity").innerHTML = "This is not a valid tweet";
+  }
+    else {
+      parseTweet(txt);
+  }
+}
+function parseTweet(txt){
+  document.getElementById("validity").innerHTML = "This is a valid tweet";
+  var tweetStrings = txt.value.split(" ");
+//  document.getElementById("numStrings").innerHTML = tweetStrings.value.length;
+//  document.getElementById("urls").innerHTML = n;
+//  var arrayLength = tweetStrings.length;
+//  document.getElementById("urls").innerHtml = urlsArray.length;
+var testTweet;
+var urlCount = 0;
+var mentionCount = 0;
+var topicCount = 0;
+var  urls =[], mentions = [], topics =[];
+for(var i = 0; i <tweetStrings.length; i++){
+  testTweet = tweetStrings[i];
+  console.log(/^@/.test(testTweet));
+  if (/^http/.test(testTweet)){
+      urlCount++;
+      urls.push(testTweet);
+  }
+  else if(/^#/.test(testTweet)){
+      topicCount++;
+      topics.push(testTweet);
+
+  }
+  else if (/^@/.test(testTweet)){
+    mentionCount++;
+    mentions.push(testTweet);
+  }
+}
+document.getElementById("mentions").innerHTML = mentionCount;
+document.getElementById("topics").innerHTML = topicCount;
+document.getElementById("urls").innerHTML = urlCount;
+}
