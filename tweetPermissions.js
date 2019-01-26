cfd8822abe2f0cdf6726c8effba7135825972e4d
Linette V Maliakal <lmaliakal21@gmail.com>
2019-01-26 14:51:26 -0600

    added/edited files from mac branch for testing
    Signed-off-by: Linette V Maliakal <lmaliakal21@gmail.com>

---
+++ tweetPermissions.js
@@ -0,0 +1,44 @@
+function testTweet(){;
+  var tweet = new Object(), txt = document.getElementById("field1");
+  var tweetLength = txt.value.length;
+  if (tweetLength > 280){
+    document.getElementById("validity").innerHTML = "This is not a valid tweet";
+    document.getElementById("length").innerHTML = txt.value.length;
+  }
+  if (tweetLength == 0){
+    document.getElementById("validity").innerHTML = "This is not a valid tweet";
+  }
+    else {
+      parseTweet(txt);
+  }
+  document.getElementById("length").innerHTML = "Length of tweet: " + tweetLength;
+}
+function parseTweet(txt){
+  document.getElementById("validity").innerHTML = "This is a valid tweet";
+  var tweetStrings = txt.value.split(" ");
+  var testTweet;
+  var urlCount = 0;
+  var mentionCount = 0;
+  var topicCount = 0;
+  var  urls =[], mentions = [], topics =[];
+  for(var i = 0; i <tweetStrings.length; i++){
+    testTweet = tweetStrings[i];
+    console.log(/^@/.test(testTweet));
+    if (/^http/.test(testTweet)){
+      urlCount++;
+      urls.push(testTweet);
+    }
+    else if(/^#/.test(testTweet)){
+      topicCount++;
+      topics.push(testTweet);
+    }
+    else if (/^@/.test(testTweet)){
+      mentionCount++;
+      mentions.push(testTweet);
+    }
+  }
+  document.getElementById("mentions").innerHTML = "Number of mentions: " + mentionCount;
+  document.getElementById("topics").innerHTML = "Number of topics: " + topicCount;
+  document.getElementById("urls").innerHTML = "Number of URLs: " + urlCount;
+  document.getElementById("numStrings").innerHTML = "Number of strings: " + tweetStrings.length;
+}
