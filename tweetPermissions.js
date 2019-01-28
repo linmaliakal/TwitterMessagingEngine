5a60c4ff8f1f5a08fe4fecf577c86b1c9e795cf4
Linette V Maliakal <lmaliakal21@gmail.com>
2019-01-28 12:27:15 -0600

    adding Mikey's css file and new js updates, fixed over 280 validity of tweet
    Signed-off-by: Linette V Maliakal <lmaliakal21@gmail.com>

--- tweetPermissions.js
+++ tweetPermissions.js
@@ -1,5 +1,8 @@
 function tLength(){
   var txt = document.getElementById("field1");
+  parseTweet(txt);
+}
+/*
   var tweetLength = txt.value.length;
   if (tweetLength >= 280){
     document.getElementById("validity").innerHTML = "This is not a valid tweet";
@@ -13,29 +16,38 @@ function tLength(){
   }
   document.getElementById("length").innerHTML = "Length of tweet: " + tweetLength;
 }
+*/
 function parseTweet(txt){
-  document.getElementById("validity").innerHTML = "This is a valid tweet";
   var tweetStrings = txt.value.split(" ");
+  var tweetLength = txt.value.length;
   var testTweet;
   var urlCount = 0;
   var mentionCount = 0;
   var topicCount = 0;
   var  urls =[], mentions = [], topics =[];
-  for(var i = 0; i <tweetStrings.length; i++){
-    testTweet = tweetStrings[i];
-    console.log(/^@/.test(testTweet));
-    if (/^http/.test(testTweet)){
-      urlCount++;
-      urls.push(testTweet);
+  if (tweetLength <= 280 && tweetLength != 0){
+    document.getElementById("validity").innerHTML = "This is a valid tweet";
+    document.getElementById("length").innerHTML = "The length of this text is "+ txt.value.length + " total characters";
+    for(var i = 0; i <tweetStrings.length; i++){
+      testTweet = tweetStrings[i];
+      if (/^http/.test(testTweet)){
+        urlCount++;
+        urls.push(testTweet);
+      }
+      else if(/^#/.test(testTweet)){
+        topicCount++;
+        topics.push(testTweet);
+      }
+      else if (/^@/.test(testTweet)){
+        mentionCount++;
+        mentions.push(testTweet);
+      }
     }
-    else if(/^#/.test(testTweet)){
-      topicCount++;
-      topics.push(testTweet);
-    }
-    else if (/^@/.test(testTweet)){
-      mentionCount++;
-      mentions.push(testTweet);
-    }
+  } else if (tweetLength == 0 || tweetLength > 280){
+    document.getElementById("validity").innerHTML = "This is not a valid tweet";
+  } else {
+    document.getElementById("validity").innerHTML = "This is not a valid tweet";
+    document.getElementById("length").innerHTML = "The length of this text is "+ txt.value.length +" total characters";
   }
   document.getElementById("mentions").innerHTML = "Number of mentions: " + mentionCount;
   document.getElementById("topics").innerHTML = "Number of topics: " + topicCount;
@@ -45,3 +57,27 @@ function parseTweet(txt){
 function getOpposite(foo) {
   return !foo;
 }
+/* Old Version parseTweet
+document.getElementById("validity").innerHTML = "This is a valid tweet";
+var tweetStrings = txt.value.split(" ");
+var testTweet;
+var urlCount = 0;
+var mentionCount = 0;
+var topicCount = 0;
+var  urls =[], mentions = [], topics =[];
+for(var i = 0; i <tweetStrings.length; i++){
+  testTweet = tweetStrings[i];
+  console.log(/^@/.test(testTweet));
+  if (/^http/.test(testTweet)){
+    urlCount++;
+    urls.push(testTweet);
+  }
+  else if(/^#/.test(testTweet)){
+    topicCount++;
+    topics.push(testTweet);
+  }
+  else if (/^@/.test(testTweet)){
+    mentionCount++;
+    mentions.push(testTweet);
+  }
+} */
