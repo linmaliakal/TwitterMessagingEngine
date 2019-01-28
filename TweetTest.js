ca3f66ff9c4745b385cfc0f1f1be3e65d3a52fc1
macabrera9 <mcabrera2@luc.edu>
2019-01-28 14:10:30 -0600

    at the TA's suggestion, now trying to iterate through each string as a set of substrings to better differentiate between proper mentions and topics
    Signed-off-by: macabrera9 <mcabrera2@luc.edu>

--- TweetTest.js
+++ TweetTest.js
@@ -1,5 +1,5 @@
 function testTweet(){;
-//  var tweet = new Object(), 
+//  var tweet = new Object(),
   var txt = document.getElementById("field1");
   parseTweet(txt);
 }
@@ -11,22 +11,41 @@ function parseTweet(txt){
   var mentionCount = 0;
   var topicCount = 0;
   var  urls =[], mentions = [], topics =[];
+  var  urlsFinal =[], mentionsFinal = [], topicsFinal =[];
   if (tweetLength < 280 && tweetLength != 0){
     document.getElementById("validity").innerHTML = "This is a valid tweet";
     document.getElementById("length").innerHTML = "The length of this text is "+ txt.value.length + " total characters";
     for(var i = 0; i < tweetStrings.length; i++){
     testTweet = tweetStrings[i];
-      if (/^http/.test(testTweet)){
-        urlCount++;
+      if (/http{1}/.test(testTweet)){
         urls.push(testTweet);
     }
-      else if(/^#{1}/.test(testTweet)){
-        topicCount++;
+      else if(/#{1}/.test(testTweet)){
         topics.push(testTweet);
     }
-      else if (/^@{1}/.test(testTweet)){
-      mentionCount++;
+      else if (/@{1}/.test(testTweet)){
       mentions.push(testTweet);
+    }
+  }
+  for(var a = 0; a< urls.length; a++){
+    testerTweet1 = urls[a];
+    if (/^http/.test(testerTweet1)){
+      urlCount++;
+      urlsFinal.push(testerTweet1);
+    }
+  }
+  for(var b = 0; b< mentions.length; b++){
+    testerTweet2 = mentions[b];
+    if (/^@/.test(testerTweet2)){
+      mentionCount++;
+      mentionFinal.push(testerTweet2);
+    }
+  }
+  for(var c = 0; c< topics.length; c++){
+    testerTweet3 = topics[c];
+    if (/^#/.test(testerTweet3)){
+      urlCount++;
+      urlsFinal.push(testerTweet);
     }
   }
 } else if (tweetLength == 0 || tweetLength > 280){
