e3d209f639570d06a28e042990375a1247ee9895
Linette V Maliakal <lmaliakal21@gmail.com>
2019-01-27 21:05:36 -0600

    adding tests for max length of tweet
    Signed-off-by: Linette V Maliakal <lmaliakal21@gmail.com>

--- tweetPermissionsSpec.js
+++ tweetPermissionsSpec.js
@@ -1,8 +1,42 @@
-describe("tLength", function() {
-  it("should be equal to or less than 280 characters", function(){
+//trial unit test
+describe("Opposite Value", function() {
+  it("Returns opposite value of given variable, using to check Jasmine running", function() {
+    //arrange to test against
+    let foo = false;
+    //act the function
+    const result = getOpposite(foo);
+    //assert to check result with expected
+    expect(result).toBe(true);
+  });
+});
+describe("Length of Tweet", function() {
+  it("Only allows tweets that are greater than or equal to 280 characters", function() {
+    //arrange
+    var charLength = 530;
+    //act
+    var result = charLength;
+    //assert
+    expect(result).toBeGreaterThan(280);
+  });
+});
+describe("Mentions from @", function() {
+  it("Recognizes any string after @ symbol to be a mention of another account/person", function() {
     //arrange
     //act
     //assert
-    expect(result).toBe(true);
+  });
+});
+describe("Topics from #", function() {
+  it("Recognizes any string after # to be a topic except if # follows first #", function() {
+    //arrange
+    //act
+    //assert
+  });
+});
+describe("URLs", function() {
+  it("Finds URLs in the tweet and shows them as such", function() {
+    //arrange
+    //act
+    //assert
   });
 });
