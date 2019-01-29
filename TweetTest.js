02401ec9f8e2749467f7446b287f9e02114d2a5a
macabrera9 <mcabrera2@luc.edu>
2019-01-28 18:06:05 -0600

    program now only counts topics, links, and mentions with 1 respective symbol at last
    Signed-off-by: macabrera9 <mcabrera2@luc.edu>

--- TweetTest.js
+++ TweetTest.js
@@ -1,5 +1,4 @@
 function testTweet(){;
-//  var tweet = new Object(),
   var txt = document.getElementById("field1");
   parseTweet(txt);
 }
@@ -10,6 +9,12 @@ function parseTweet(txt){
   var urlCount = 0;
   var mentionCount = 0;
   var topicCount = 0;
+  var url = RegExp('^https://');
+  var mention = RegExp('^@');
+  var topic = RegExp('^#');
+  var urlRec = RegExp('https://{2}');
+  var mentionRec = RegExp('@{2}');
+  var topicRec = RegExp('#{2}');
   var  urls =[], mentions = [], topics =[];
   var  urlsFinal =[], mentionsFinal = [], topicsFinal =[];
   if (tweetLength < 280 && tweetLength != 0){
@@ -17,42 +22,40 @@ function parseTweet(txt){
     document.getElementById("length").innerHTML = "The length of this text is "+ txt.value.length + " total characters";
     for(var i = 0; i < tweetStrings.length; i++){
     testTweet = tweetStrings[i];
-      if (/http{1}/.test(testTweet)){
+      if (url.test(testTweet)){
         urls.push(testTweet);
     }
-      else if(/#{1}/.test(testTweet)){
+      else if(topic.test(testTweet)){
         topics.push(testTweet);
     }
-      else if (/@{1}/.test(testTweet)){
-      mentions.push(testTweet);
+      else if (mention.test(testTweet)){
+        mentions.push(testTweet);
     }
   }
   for(var a = 0; a< urls.length; a++){
     testerTweet1 = urls[a];
-    if (/^http/.test(testerTweet1)){
+    if (urlRec.test(testerTweet1) == false){
       urlCount++;
       urlsFinal.push(testerTweet1);
     }
   }
   for(var b = 0; b< mentions.length; b++){
     testerTweet2 = mentions[b];
-    if (/^@/.test(testerTweet2)){
+    if (mentionRec.test(testerTweet2) == false){
       mentionCount++;
-      mentionFinal.push(testerTweet2);
+      mentionsFinal.push(testerTweet2);
     }
   }
   for(var c = 0; c< topics.length; c++){
     testerTweet3 = topics[c];
-    if (/^#/.test(testerTweet3)){
-      urlCount++;
-      urlsFinal.push(testerTweet);
+    if (topicRec.test(testerTweet3) == false){
+      topicCount++;
+      topicsFinal.push(testerTweet3);
     }
   }
 } else if (tweetLength == 0 || tweetLength > 280){
     document.getElementById("validity").innerHTML = "This is not a valid tweet";
-  } else {
-      document.getElementById("validity").innerHTML = "This is not a valid tweet";
-      document.getElementById("length").innerHTML = "The length of this text is "+ txt.value.length +" total characters";
+    document.getElementById("length").innerHTML = "The length of this text is "+ txt.value.length;
   }
 
 document.getElementById("mentions").innerHTML = "This is the number of mentions " + mentionCount; //+ ". They are " + mentions;
